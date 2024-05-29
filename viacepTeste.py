372f62fd04b28d7a519da9e6b879293ebefcf800
André Judá <judatrainer@gmail.com>
2024-05-28 23:14:14 -0300

    28 5 24

--- viacepTeste.py
+++ viacepTeste.py
@@ -1,46 +1,33 @@
-"""
-from flask import Flask, jsonify
-import requests
-
-api = Flask(__name__)
-
-@api.route('/')
-def indice():
-      cep = input('Digite o cep: ')
-      dados = f'https://viacep.com.br/ws/{cep}/json/'
-      requisicao = requests.get(dados)
-      print(requisicao)
-      print(requisicao.json())
-
-
-if __name__ == '__main__':
-      api.run()
-
-#endpoint fazer requisicoes no link"""
 
 from flask import Flask
 import requests
-import xmltodict
 
+print('')
 cep = input('Digite o cep: ')
 dadosCep = f'https://viacep.com.br/ws/{cep}/json/'
 requisicao = requests.get(dadosCep)
-print('aqui', requisicao)
+#print('aqui', requisicao)
 
 dicionario_requisicao = requisicao.json()
 print(dicionario_requisicao) #apenas salvou o json numa biblioteca e printou 
 
 uf = dicionario_requisicao['uf']
-print(uf)
+#print(uf) = SP
 
-localidade = dicionario_requisicao['localidade'].lower().replace('ã', 'a').replace(' ','%20')
-print(localidade)
+localidade = dicionario_requisicao['localidade'] #.lower().replace('ã', 'a').replace(' ','%20')
+#print(localidade) = sao%20paulo
 
-dadosInpe = f'http://servicos.cptec.inpe.br/XML/listaCidades?city={localidade}'
+key = 'd62f164eb8183f37dd03f6ac4eb0fcd9'
 
-requisicao2 = requests.get(dadosInpe)
-print('aqui2', requisicao)
+site = f'https://api.openweathermap.org/data/2.5/weather?q={localidade}&appid={key}&lang=pt_br'
+req = requests.get(site)
+#print('aqui2', req)
 
-dic_inpe = requisicao2.text
-
-print(dic_inpe)
+dic = req.json()
+#print(dic)
+tempo = dic['weather'][0]['description']
+temperatura = dic['main']['temp'] - 273.15
+sensacao = dic['main']['feels_like'] - 273.15
+print(f'Tempo hoje = {tempo},' f' Temperatura de {temperatura:.0f}º, sensação de {sensacao:.0f}°')
+print('')
+print()
